857619336e015b2a50a6253db82d25e5e88a5e78
shahchhatru <shahkhesavchhatra@gmail.com>
2023-08-07 06:46:44 +0545

    User signup Creation and User Authentication implemented

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # social-media-clone
+
+## Description
+This is clone of social media like application using django and front-end technologies like html,css and js. Here I have completed the login page
+
+
+## Setup and Installation
+To install all project dependencies, run the following command:
+
+```bash
+pipenv install
+```
+
+##Activate the Environment
+
+To activate the virtual environment, use the following command:
+
+```bash
+pipenv shell
+```
+
+##Running the Project
+Navigate to the project directory:
+
+```bash
+cd social-book
+```
+
+To run the project, execute the following command:
+
+```bash
+pipenv run python3 manage.py runserver
+
+```
+
+
+![Email validation](./images/loginscreenview.png)
+
+![Email validation](./images/EmailError.png)
+
+![password validation](./images/passwordLengthAuthentication.png)
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+##let's import user model to create user for our app
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
+from .models import Profile
+
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+def signup(request):
+    if request.method=="POST":
+        #let's get all the values passed using post method
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+        if password == password2:
+            #check whether email is already taken by user or not
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists(): #check whether username is already taken
+                messages.info(request,'Username Taken')
+                return redirect('signup')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+
+                #log user in and redirect to settings page
+
+                #create a profile object for the newuser
+                user_model = User.objects.get(username=username)
+                new_profile= Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request,'Password Not Matching')
+            return redirect('signup')
+
+
+    else:
+        return render(request,'signup.html')
